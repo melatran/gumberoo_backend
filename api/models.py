44fed9419e558dabd8419bf80caa6331765e7a41
Melanie Tran <melaniexx02@gmail.com>
2020-09-05 15:47:57 -0600

    Make teachers

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Teacher(models.Model):
+  first_name = models.CharField
+  last_name = models.CharField
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
