83b332c13329078d103fa07f5cd4a3cc856a6642
Melanie Tran <melaniexx02@gmail.com>
2020-09-05 15:59:17 -0600

    Add fields to teacher

--- api/models.py
+++ api/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Teacher(models.Model):
-  first_name = models.CharField
-  last_name = models.CharField
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+
