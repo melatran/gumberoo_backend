23fc1453dc8938c292be24829b889715b7b13cb9
Derek Borski <derek.borski@gmail.com>
2020-09-05 13:03:11 -0600

    Change database settings to postgresql

--- gumberoo_backend/settings.py
+++ gumberoo_backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '118*x^6^)*am@ul$@obh7p!+e@3^whn95g-4#1hyzi_)zjfw62'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +78,10 @@ WSGI_APPLICATION = 'gumberoo_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gumberoo',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
