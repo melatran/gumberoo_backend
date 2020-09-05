0e93de3b5b5fc361bbbb40bd0f6a66ea7bae6f5c
Melanie Tran <melaniexx02@gmail.com>
2020-09-05 16:12:54 -0600

    Create students

--- api/models.py
+++ api/models.py
@@ -8,3 +8,10 @@ class Teacher(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
 
+class Student(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  age = models.IntegerField
+  teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
