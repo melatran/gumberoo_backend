492b8b2062445a44e554d58b939e585d61cd7768
Derek Borski <derek.borski@gmail.com>
2020-09-07 16:32:15 -0600

    Add port 5432 to database information

--- gumberoo_backend/settings.py
+++ gumberoo_backend/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'gumberoo',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
 
