a7a67c580037381dc299c1fb80db154ef2ce573f
Derek Borski <derek.borski@gmail.com>
2020-09-06 23:18:58 -0600

    Update TeacherList and TeacherDetail classes to correctly handle requests and return correct response

--- api/views.py
+++ api/views.py
@@ -6,7 +6,18 @@ from rest_framework.response import Response
 from api.models import Teacher
 from api.serializers import TeacherSerializer
 
-class TeacherList(generics.ListAPIView):
+
+class TeacherList(generics.CreateAPIView, generics.ListAPIView):
   queryset = Teacher.objects.all()
   serializer_class = TeacherSerializer
+
   
+class TeacherDetail(generics.RetrieveAPIView):
+  queryset = Teacher.objects.all()
+  serializer_class = TeacherSerializer
+
+  def get(self, request, id):
+    teacher = Teacher.objects.get(pk=id)
+
+    serializer = TeacherSerializer(teacher)
+    return Response(serializer.data)
