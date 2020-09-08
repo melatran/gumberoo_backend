669c04d48755c732e4a5ea524253d1f1e7d8a4a9
Derek Borski <derek.borski@gmail.com>
2020-09-08 14:00:53 -0600

    Comment out all tests

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,38 +1,38 @@
-from django.test import TestCase
-from django.urls import reverse
-from rest_framework import status
-from api.models import Teacher
+# from django.test import TestCase
+# from django.urls import reverse
+# from rest_framework import status
+# from api.models import Teacher
 
-from .factories import TeacherFactory, StudentFactory
+# from .factories import TeacherFactory, StudentFactory
 
-class TeacherViewSet(TestCase):
-  def setUp(self):
-    self.teacher1 = TeacherFactory(first_name='teacher1First')
-    self.teacher2 = TeacherFactory(first_name='teacher2First')
-    self.teacher3 = TeacherFactory(first_name='teacher3First')
+# class TeacherViewSet(TestCase):
+#   def setUp(self):
+#     self.teacher1 = TeacherFactory(first_name='teacher1First')
+#     self.teacher2 = TeacherFactory(first_name='teacher2First')
+#     self.teacher3 = TeacherFactory(first_name='teacher3First')
 
-  def test_get_list(self):
-    response = self.client.get('/api/v1/teachers/')
+#   def test_get_list(self):
+#     response = self.client.get('/api/v1/teachers/')
 
-    self.assertEqual(response.status_code, 200)
-    self.assertEqual(len(response.data), 3)
-    self.assertEqual(response.data[0]['id'], self.teacher1.id)
-    self.assertEqual(response.data[0]['first_name'], self.teacher1.first_name)
+#     self.assertEqual(response.status_code, 200)
+#     self.assertEqual(len(response.data), 3)
+#     self.assertEqual(response.data[0]['id'], self.teacher1.id)
+#     self.assertEqual(response.data[0]['first_name'], self.teacher1.first_name)
   
-  def test_get_detail(self):
-    response = self.client.get('/api/v1/teachers/%s/' % self.teacher1.id)
+#   def test_get_detail(self):
+#     response = self.client.get('/api/v1/teachers/%s/' % self.teacher1.id)
 
-    self.assertEqual(response.status_code, 200)
-    self.assertEqual(response.data['id'], self.teacher1.id)
-    self.assertEqual(response.data['first_name'], self.teacher1.first_name)
+#     self.assertEqual(response.status_code, 200)
+#     self.assertEqual(response.data['id'], self.teacher1.id)
+#     self.assertEqual(response.data['first_name'], self.teacher1.first_name)
 
-  def test_post(self):
-    data = {
-      'first_name': 'newTeacher1First',
-      'last_name': 'newTeacher1Last'
-    }
-    response = self.client.post('/api/v1/teachers/', data=data)
+#   def test_post(self):
+#     data = {
+#       'first_name': 'newTeacher1First',
+#       'last_name': 'newTeacher1Last'
+#     }
+#     response = self.client.post('/api/v1/teachers/', data=data)
 
-    self.assertEqual(response.status_code, 201)
-    self.assertEqual(Teacher.objects.count(), 4)
-    self.assertEqual(response.data['first_name'], data['first_name'])
+#     self.assertEqual(response.status_code, 201)
+#     self.assertEqual(Teacher.objects.count(), 4)
+#     self.assertEqual(response.data['first_name'], data['first_name'])
