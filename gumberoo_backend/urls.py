89c9b108a6282d0360620bb1ac5bb99fefc536bd
Derek Borski <derek.borski@gmail.com>
2020-09-08 22:23:11 -0600

    Add api/v1/teachers/:teacher_id/lessons route

--- gumberoo_backend/urls.py
+++ gumberoo_backend/urls.py
@@ -21,7 +21,8 @@ from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/teachers/', views.TeacherList.as_view()),
-    path('api/v1/teachers/<int:pk>/', views.TeacherDetail.as_view())
+    path('api/v1/teachers/<int:pk>/', views.TeacherDetail.as_view()),
+    path('api/v1/teachers/<int:pk>/lessons/', views.TeacherLesson.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
