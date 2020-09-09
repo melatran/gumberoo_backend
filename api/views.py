fdc245253cf78d74ec2e24e47ede8961d0f823b6
Derek Borski <derek.borski@gmail.com>
2020-09-08 22:23:42 -0600

    Add TeacherLesson class and post function to handle teacher lesson post requests

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, QueryDict
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
 
 from api.models import Teacher
 from api.serializers import TeacherSerializer
@@ -21,3 +22,24 @@ class TeacherDetail(generics.RetrieveAPIView):
 
   #   serializer = TeacherSerializer(teacher)
   #   return Response(serializer.data)
+
+class TeacherLesson(APIView):
+  parser_classes = [JSONParser]
+
+  def post(self, request, pk):
+    teacher = Teacher.objects.get(pk=pk)
+
+    new_lesson = teacher.lesson_set.create(name=request.data['lesson']['name'])
+
+    for question in request.data['lesson']['questions']:
+      new_question = new_lesson.question_set.create(
+        question=question['desc'],
+        reading=question['reading']
+      )
+      for answer in question['answers']:
+        new_question.answer_set.create(
+          answer=answer['desc'],
+          correct=answer['correct']
+        )
+
+    import code; code.interact(local=dict(globals(), **locals()))
