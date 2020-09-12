b6eb352a84bd1af2fcf54b6ee508d95e8757b11e
Derek Borski <derek.borski@gmail.com>
2020-09-12 12:29:48 -0600

    Add localhost 8000 to allowed domains

--- gumberoo_backend/config/settings/base.py
+++ gumberoo_backend/config/settings/base.py
@@ -61,7 +61,9 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
 ]
 
 ROOT_URLCONF = 'gumberoo_backend.urls'
