1d77edac3cc7010ca6bbe7bd3981840d87e89d5a
Melanie Tran <melaniexx02@gmail.com>
2020-09-12 14:10:36 -0600

    Hide api key

--- api/watson_service.py
+++ api/watson_service.py
@@ -1,13 +1,16 @@
 import json
+import os
+import requests
+from dotenv import load_dotenv, find_dotenv
 from ibm_watson import ToneAnalyzerV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-authenticator = IAMAuthenticator(
-    'C5A_uO1F6b3ncrvWf5uSmR0wwrAJhrGN2LxfgICO5udJ')
+load_dotenv(find_dotenv())
+
+tone_analyzer_authenticator = IAMAuthenticator(os.environ.get('WATSON_APIKEY'))
 tone_analyzer = ToneAnalyzerV3(
     version='2017-09-21',
-    authenticator=authenticator
-)
+    authenticator=tone_analyzer_authenticator)
 
 tone_analyzer.set_service_url(
     'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/bbe286c2-3d3c-4fdc-ae5e-89f0419c10cb')
