205f864eba40ebfab35547896934e10bc171d9b2
Derek Borski <derek.borski@gmail.com>
2020-09-15 17:40:44 -0600

    Create ZScoreGenerator popo that encapsulates this logic into it's own method. Refactor view to now use this generator to dry up code in the controller

--- api/popos.py
+++ api/popos.py
@@ -1,3 +1,6 @@
+import numpy as np
+import scipy.stats as stats
+
 class StudentScore:
   def __init__(self, student_id, average_score):
     self.student_id = student_id
@@ -12,3 +15,22 @@ class ZScore:
   def __init__(self, student_id, zscore):
     self.student_id = student_id
     self.zscore = zscore
+
+class ZScoreGenerator:
+  def __init__(self, lesson_students):
+    self.lesson_students = lesson_students
+
+  def generate_zscore(self):
+    scores = {}
+    for lesson_student in self.lesson_students:
+      scores[lesson_student.student_id] = lesson_student.score
+
+    keys, vals = zip(*scores.items())
+    zscore = stats.zscore(vals)
+    new_scores = dict(zip(keys, zscore))
+
+    zscores = []
+    for student_id, score in new_scores.items():
+      zscores.append(ZScore(student_id=student_id, zscore=score))
+    
+    return zscores
